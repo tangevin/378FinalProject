153d30937fb70eeb32ca710d389eac37dfc6c919
Nate Diamond <nate.diamond13@gmail.com>
2016-02-25 14:00:07 -0800

    Added grid and some functionality (tiles can get range of tiles in immediate vicinity)

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,20 +1,47 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
-public class Tile : MonoBehaviour {
+public class Tile : MonoBehaviour
+{
+    private bool active;
+    private SpriteRenderer sprite;
+    private GameObject world;
+    public int x { get; private set; }
+    public int y { get; private set; }
+
+    public int range;
     public BaseHumidity baseHumidity;
     public BaseTemperature baseTemperature;
     public BiomeType biomeType;
     public HumidityVariation humidityVariation;
     public TemperatureVariation temperatureVariation;
 
-    // Use this for initialization
-    void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        active = false;
+    }
+
+    void OnMouseDown()
+    {
+        List<GameObject> tiles = new List<GameObject>();
+        world.GetComponent<World>().GetTilesInRange(tiles, x, y, range);
+
+        foreach (GameObject tile in tiles)
+        {
+            tile.GetComponent<Tile>().ChangeColor();
+        }
+    }
+
+    public void SetData(int x, int y, GameObject world)
+    {
+        this.x = x;
+        this.y = y;
+        this.world = world;
+    }
+
+    private void ChangeColor()
+    {
+        sprite.color = (active = !active) ? Color.green : Color.white;
+    }
 }
--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -1,16 +1,116 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
-public class World : MonoBehaviour {
-    private Tile[][] map;
+public class World : MonoBehaviour
+{
+    GameObject[,] map;
+    public int height, width;
+    public GameObject tilePrefab, worldPrefab;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 pos;
+        map = new GameObject[height, width];
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (y % 2 == 0)
+                {
+                    pos = new Vector3((x - 0.25f - width / 2) * .5f, (y - height / 2) * .375f);
+                }
+                else
+                {
+                    pos = new Vector3((x + 0.25f - width / 2) * .5f, (y - height / 2) * .375f);
+                }
+
+                GameObject go = (GameObject)Instantiate(tilePrefab, pos, Quaternion.identity);
+                go.GetComponent<Tile>().SetData(x, y, worldPrefab);
+                map[y, x] = go;
+            }
+        }
+    }
+
+    public void GetTilesInRange(List<GameObject> list, int x, int y, int range)
+    {
+        // Add the current tile
+        list.Add(map[y, x]);
+
+        if (range > 0)
+        {
+            // Get the first ring of tiles adjacent to the current tile
+            GetImmediateAdjacentTiles(list, x, y);
+            for (int rng = 0; rng < range - 1; rng++)
+            {
+                // Only check the tiles that were added from the previous ring
+                int size = list.Count;
+                for (int index = 0; index < size; index++)
+                {
+                    // Get each consecutive ring of adjacent tiles
+                    Tile temp = list[index].GetComponent<Tile>();
+                    GetImmediateAdjacentTiles(list, temp.x, temp.y);
+                }
+            }
+        }
+    }
+
+    private void GetImmediateAdjacentTiles(List<GameObject> list, int x, int y)
+    {
+        // Same and left
+        if (x > 0)
+        {
+            AddConditionally(list, map[y, x - 1]);
+        }
+        // Same and right
+        if (x < width - 1)
+        {
+            AddConditionally(list, map[y, x + 1]);
+        }
+
+        // Row Above
+        if (y > 0)
+        {
+            // Immediately above
+            AddConditionally(list, map[y - 1, x]);
+
+            if (y % 2 == 1 && x < width - 1)
+            {
+                // Above and right
+                AddConditionally(list, map[y - 1, x + 1]);
+            }
+            else if (y % 2 == 0 && x > 0)
+            {
+                // Above and left
+                AddConditionally(list, map[y - 1, x - 1]);
+            }
+        }
+
+        // Row Below
+        if (y < height - 1)
+        {
+            // Immediately below
+            AddConditionally(list, map[y + 1, x]);
+
+            if (y % 2 == 1 && x < width - 1)
+            {
+                // Below and right
+                AddConditionally(list, map[y + 1, x + 1]);
+            }
+            else if (y % 2 == 0 && x > 0)
+            {
+                // Below and left
+                AddConditionally(list, map[y + 1, x - 1]);
+            }
+        }
+    }
+
+    private void AddConditionally(List<GameObject> list, GameObject tile)
+    {
+        // Only add a tile to the list if it wasn't there before
+        if (!list.Contains(tile))
+        {
+            list.Add(tile);
+        }
+    }
 }

4bb33f4cb5c1d2e6707d4a034f3e426664231ac6
Alexander <alexandazar@gmail.com>
2016-03-17 16:25:50 -0700

    Made object selector resize with selection. Added sprites to plant selector.

--- Assets/Scripts/ParameterDropdownHandling.cs
+++ Assets/Scripts/ParameterDropdownHandling.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ParameterDropdownHandling : MonoBehaviour
 {
@@ -12,7 +13,22 @@ public class ParameterDropdownHandling : MonoBehaviour
     {
         animalParameters.SetActive(!animalsSelected);
         plantParameters.SetActive(animalsSelected);
-
         animalsSelected = !animalsSelected;
+
+        Image background = GameObject.Find("Object Select Background").GetComponent<Image>();
+        RectTransform backSpot = background.GetComponent<RectTransform>();
+        InputField speciesNameField = GameObject.Find("Species Name").GetComponent<InputField>();
+        RectTransform fieldSpot = speciesNameField.GetComponent<RectTransform>();
+
+        if (animalsSelected)
+        {
+            fieldSpot.anchoredPosition = new Vector2(fieldSpot.anchoredPosition.x, -475);
+            backSpot.anchoredPosition = new Vector2(backSpot.anchoredPosition.x, 0);
+        } else
+        {
+            fieldSpot.anchoredPosition = new Vector2(fieldSpot.anchoredPosition.x, -365);
+            backSpot.anchoredPosition = new Vector2(backSpot.anchoredPosition.x, 110);
+        }
+
     }
 }
